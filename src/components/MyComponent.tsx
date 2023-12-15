1a314b1b911ce3c6dd0852210d560969039875fa
Roderick Mendoza <rodmendoza07@gmail.com>
2023-12-15 10:05:28 -0800

    created button

---
+++ src/components/MyComponent.tsx
@@ -0,0 +1,13 @@
+'use client';
+import './MyComponent.css';
+
+export default function MyComponent () {
+
+  return (
+    <div className="centered-div">
+      <button className = "button">
+         <text className="button-text">Discover more of our work</text>
+      </button>
+    </div>
+  )
+}
